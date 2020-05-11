26ca31fd8da0cf44baa8f3209c64a69353bbdfa9
Rodrigo <=>
2020-05-11 20:23:58 -0300

    j

--- php/mailer.php
+++ php/mailer.php
@@ -1,42 +1,17 @@
 <?php
-
-/* Namespace alias. */
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'src/Exception.php';
-require 'src/PHPMailer.php';
-require 'src/SMTP.php';
-
-/* Create a new PHPMailer object. Passing TRUE to the constructor enables exceptions. */
-$mail = new PHPMailer(TRUE);
-
-/* Open the try/catch block. */
-try {
-   /* Set the mail sender. */
-   $mail->setFrom('info@lupenni.com', 'Lupenni Studios');
-
-   /* Add a recipient. */
-   $mail->addAddress('rodrigo19-1_1998@hotmail.com', 'Rodrigo');
-
-   /* Set the subject. */
-   $mail->Subject = 'Force';
-
-   /* Set the mail message body. */
-   $mail->Body = 'There is a great disturbance in the Force.';
-
-   /* Finally send the mail. */
-   $mail->send();
-}
-catch (Exception $e)
+$from = 'Darth Vader <info@lupenni.com>';
+$to = 'Emperor <rodrigo19-1_1998@hotmail.com>';
+$subject = 'Force';
+$message = 'There is a great disturbance in the Force.';
+$headers = 'From: ' . $from;
+ 
+if (!mail($to, $subject, $message, $headers))
 {
-   /* PHPMailer exception. */
-   echo $e->errorMessage();
+    echo "Error.";
 }
-catch (\Exception $e)
+else
 {
-   /* PHP exception (note the backslash to select the global namespace Exception class). */
-   echo $e->getMessage();
+    echo "Message sent.";
 }
 
 ?>
