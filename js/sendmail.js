93328d6f75c0187a726f15ac7035eb8db2d9439a
Rodrigo <=>
2020-05-11 19:05:03 -0300

    k

---
+++ js/sendmail.js
@@ -0,0 +1,31 @@
+function sendmail() {
+    var nombre = document.getElementById('nombre-contacto-form').value;
+    var mail = document.getElementById('mail-contacto-form').value;
+    var asunto = document.getElementById('asunto-contacto-form').value;
+    var telefono = document.getElementById('telefono-contacto-form').value;
+    var mensaje = document.getElementById('mensaje-contacto-form').value;
+
+    var str = {
+        nombre: nombre,
+        mail: mail,
+        asunto: asunto,
+        telefono: telefono,
+        mensaje: mensaje
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "../php/mailer.php",
+        data: str,
+        success: function(msg){
+           // alert(msg);
+            if(msg == 'OK') {
+                alert('Mensaje enviado con exito');
+            }
+            else {
+                alert('Error al enviar el mensaje');
+            }
+            
+        }
+    });
+}
