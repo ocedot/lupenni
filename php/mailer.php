34a7d9c7a15f57c6cb0f1f8b1736b2ce7d36ca11
Rodrigo <=>
2020-05-11 20:41:29 -0300

    k

--- php/mailer.php
+++ php/mailer.php
@@ -1,17 +1,46 @@
 <?php
-$from = 'Darth Vader <info@lupenni.com>';
-$to = 'Emperor <rodrigo19-1_1998@hotmail.com>';
-$subject = 'Force';
-$message = 'There is a great disturbance in the Force.';
-$headers = 'From: ' . $from;
- 
-if (!mail($to, $subject, $message, $headers))
-{
-    echo "Error.";
-}
-else
-{
-    echo "Message sent.";
-}
 
-?>
+/* Namespace alias. */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+/* Include the Composer generated autoload.php file. */
+require 'vendor/autoload.php';
+
+/* If you installed PHPMailer without Composer do this instead: */
+/*
+require 'C:\PHPMailer\src\Exception.php';
+require 'C:\PHPMailer\src\PHPMailer.php';
+require 'C:\PHPMailer\src\SMTP.php';
+*/
+
+/* Create a new PHPMailer object. Passing TRUE to the constructor enables exceptions. */
+$mail = new PHPMailer(TRUE);
+
+/* Open the try/catch block. */
+try {
+   /* Set the mail sender. */
+   $mail->setFrom('info@lupenni.com', 'Darth Vader');
+
+   /* Add a recipient. */
+   $mail->addAddress('rodrigo19-1_1998@hotmail.com', 'Emperor');
+
+   /* Set the subject. */
+   $mail->Subject = 'Force';
+
+   /* Set the mail message body. */
+   $mail->Body = 'There is a great disturbance in the Force.';
+
+   /* Finally send the mail. */
+   $mail->send();
+}
+catch (Exception $e)
+{
+   /* PHPMailer exception. */
+   echo $e->errorMessage();
+}
+catch (\Exception $e)
+{
+   /* PHP exception (note the backslash to select the global namespace Exception class). */
+   echo $e->getMessage();
+}
